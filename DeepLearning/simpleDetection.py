108de6959c3e12e95d4fd26ccfd3b59abf3c39e0
Fab.16 <fab_16@hotmail.fr>
2021-12-18 18:25:58 +0100

    Update simpleDetection.py

--- DeepLearning/simpleDetection.py
+++ DeepLearning/simpleDetection.py
@@ -1,1 +1,116 @@
+#_______________IMPORTS_____________
+# Load dataset
+import pandas as pd
+
+# Preparation / Manipulation / Evaluation
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+# Models
+from keras.models import Sequential
+from keras.layers import Dense 
+
+
+#_______________LOAD_DATA_____________
+# Load dataset to DataFrame
+df = pd.read_csv("Pima_Indians_Diabetes.csv")
+print("Columns : ", list(df.columns), "\n")
+print("Example :\n", df.head, "\n")
+print("Total data lenght :", len(df))
+
+
+#_______________CLEANING_____________
+# Remove all empty cells by removing the row
+df.dropna(inplace=True)
+print("Data lenght after cleaning :", len(df), "\n")
+
+
+#_______________DATA_REPARTITION_____________
+# Features and labels separation (Generaly features are call X and labels Y)
+features = df.copy()
+del features["Outcome"] # delete labels collumns of feature var
+
+labels = df["Outcome"]
+
+# Count number of classes
+nbClasse = len(set(labels))
+print("Available classes :", nbClasse, "->", set(labels))
+
+
+# count the number of element by classe
+dataRepartition = {}
+listeLabels = list(labels.values)
+
+for classe in set(labels):
+  dataRepartition[str(classe)] = listeLabels.count(classe)
+
+print("Data Repartition :", dataRepartition, "\n")
+
+
+#_______________DATA_SEPARATION_____________
+# Split the dataset into 2 sets : train (70%) / test (30%)
+featureTrain, featureTest, labelTrain, labelTest = train_test_split (features, labels, test_size = 0.30, random_state = 42)
+
+trainRepartition = {}
+testRepartition = {}
+
+labelTrainList = list(labelTrain.values)
+labelTestList = list(labelTest.values)
+
+for classe in set(labels):
+  trainRepartition[classe] = labelTrainList.count(classe)
+  testRepartition[classe] = labelTestList.count(classe)
+
+print("Data Repartition after split :\ntrain :", trainRepartition, "\ntest :", testRepartition, "\n")
+
+
+# ____________________MODELES_CREATION____________
+
+featureToList = list(featureTrain.values)
+
+# number of feature for a sample (patient)
+nbFeature = len(featureToList[0]) 
+print("Number of feature for a sample", nbFeature)
+
+
+# Construct empty model
+model = Sequential() 
+
+# add input layer
+# dense = vote layer
+# 12 are the number of neurons
+# input_dim = number of feature for a sample
+model.add(Dense(12, activation = 'relu', input_dim = nbFeature)) 
+          
+# add vote layer          
+model.add(Dense(8, activation = 'relu'))
+          
+# add output layer
+model.add(Dense(nbClasse, activation = 'softmax'))
+model.compile(optimizer='rmsprop', loss='mse')
+
+
+#____________________TRAINING____________
+# Fit the model with the train features and train labels
+# batch_size represente the number of sample it will see at the same time
+# epochs represente the number of time he will see all data
+model.fit(featureTrain,labelTrain,batch_size=32,epochs=3,validation_data=(featureTest,labelTest))
+
+
+prediction = model.predict(featureTest)
+bestPred = [np.argmax(pred) for pred in prediction]
+
+# Calculate accuracy : How many prediction are good ?
+acc = accuracy_score(bestPred, labelTest)
+
+# Calculate precision : Number of correct prediction for this class / total of predictions for this class
+precision = precision_score(bestPred, labelTest)
+
+# Calculate recall : Number of correct prediction  / total element of this class
+recall = recall_score(bestPred, labelTest)
+
+# Relation beetwen precision and recall
+f1Score = f1_score(bestPred, labelTest)
+
+print("\nAccuracy:", scores*100, "\nPrecision :", precision*100, "\nRecall", recall*100, "\nF1 score", f1Score*100)
 
