d6b3314d2f8aa4696e17e1774d7c7d43f1f77f19
Fab.16 <fab_16@hotmail.fr>
2022-05-04 19:16:11 +0200

    Update simpleDetection.py

--- DeepLearning/simpleDetection.py
+++ DeepLearning/simpleDetection.py
@@ -6,13 +6,15 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
+# Matrice manipulation
+import numpy as np
+
 # Models
 from keras.models import Sequential
 from keras.layers import Dense 
 
-# Matrice manipulation
-import numpy as np
-
+# Save
+import  pickle
 
 #_______________LOAD_DATA_____________
 # Load dataset to DataFrame
